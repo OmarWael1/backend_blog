b69f85467a2f82067ef1f21ef4518ece79150c11
Ahmed Elhady <ahmed.elhady@agolo.com>
2019-10-05 20:50:34 +0200

    integrate the paintings and view painting pages with frontend

--- resources/views/reader/paintings.blade.php
+++ resources/views/reader/paintings.blade.php
@@ -1,12 +1,41 @@
 @extends('layouts.navbar')
 @section('content')
-    <main>
 
-        <section  class="container articles-container no-grid-mobile">
-           لوحات فنيه
-            <div style="height: 100%;"></div>
-        </section>
+@if(count($paints) != 0)
+<main>
+    <section class="container paintings-listing">
+        <div class="paintings-container">
+        @foreach($paints as $painting)
+            <a href="{{route('paints', ['id' => $painting->id])}}">
+                <div class="painting-item">
+                    <div class="painting-item-image">
+                        <img src="{{asset('storage/'.$painting->file_name)}}">
+                    </div>
+                    <div class="painting-desc painting-brief">
+                        <h2>
+                            {{$painting->title}}
+                        </h2>
+                        <span class="painting-data">
+                            {{$painting->description}}
+                        </span>
+                        <div class="painting-options">
+                            <span class="painting-date">
+                                {{$painting->date_of_publish}}
+                            </span>
+                            <div class="visits">
+                                عدد اللوحات
+                                <span class="count">
+                                    {{$painting->number_of_readers}}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        @endforeach
+        </div>
+    </section>
+</main>
+@endif
 
-
-    </main>
 @endsection
--- resources/views/reader/view_paint.blade.php
+++ resources/views/reader/view_paint.blade.php
@@ -1,12 +1,27 @@
 @extends('layouts.navbar')
 @section('content')
-    <main>
 
-        <section  class="container articles-container no-grid-mobile">
-           لوحات فنيه
-            <div style="height: 100%;"></div>
-        </section>
+<main>
+    <section class="container paintings">
+        <div class="painting-desc">
+            <h2></h2>
+            <span class="painting-data">
+            </span>
+            <div class="painting-options">
+                <span class="painting-date">
+                </span>
+                <div class="visits">
+                    عدد اللوحات
+                    <span class="count">
+                    </span>
+                </div>
+            </div>
+        </div>
 
-
-    </main>
+        <div class="painting">
+            <img class="painting-image" src="../images/Pantheon-770x433.jpg">
+            <p class="painting-desc-text"></p>
+        </div>
+    </section>
+</main>
 @endsection
